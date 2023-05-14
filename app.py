64e5279fe7becab6cd91fb10a314913f646df2fd
Keiseer <gabrielavieser@gmail.com>
2023-05-14 19:40:42 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+postagens = [
+    {
+        'título': 'Minha História',
+        'autor': 'Amanda Dias'
+    },
+    {
+        'título': 'Novo Dispositivo Sony',
+        'autor': 'Howard Stringer'
+    },
+    {
+        'título': 'Lançamento do Ano',
+        'autor': 'Jeff Bezos'
+    },
+]
+# Rota padrão - GET https://localhost:5000
+@app.route('/')
+def obter_postagens():
+    return jsonify(postagens)
+
+# Obter postagem por id - GET https://localhost:5000/postagem/1
+@app.route('/postagem/<int:indice>', methods=['GET'])
+def obter_postagem_por_indice(indice):
+    return jsonify(postagens[indice])
+
+# Criar uma nova postagem - POST https://localhost:5000/postagem
+@app.route('/postagem',methods=['POST'])
+def nova_postagem():
+    postagem = request.get_json()
+    postagens.append(postagem)
+
+    return jsonify(postagem, 200)
+
+# Alterar uma postagem existente - PUT https://localhost:5000/postagem/1
+@app.route('/postagem/<int:indice>',methods=['PUT'])
+def alterar_postagem(indice):
+    postagem_alterada = request.get_json()
+    postagens[indice].update(postagem_alterada)
+
+    return jsonify(postagens[indice], 200)
+
+# Excluir uma postagem - DELETE - https://localhost:5000/postagem/1
+def excluir_postagem(indice):
+    try:
+        if postagens[indice] is not None:
+            del postagens[indice]
+            return jsonify(f'Foi excluído a postagem {postagens[indice]}',200)
+    except:
+        return jsonify('Não foi possível encontrar a postagem para exclusão',404)
+
+
+@app.route('/autores')
+def obter_autores():
+    pass
+
+@app.route('/autores/<int:id_autor>',methods=['GET'])
+def obter_autor_por_id(id_autor):
+    pass
+
+@app.route('/autores',methods=['POST'])
+def novo_autor():
+    pass
+
+@app.route('/autores/<int:id_autor>',methods=['PUT'])
+def alterar_autor(id_autor):
+    pass
+
+@app.route('/autores/<int:id_autor>',methods=['DELETE'])
+def excluir_autor(id_autor):
+    pass
+
+
+app.run(port=5000,host='localhost',debug=True)
